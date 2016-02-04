0da64482b4e36f7d1961adf2106705587eae282e
Scott Steinhorst <scott.steinhorst@gmail.com>
2016-02-04 00:41:20 -0700

    Checks for valid image types, compares image size and does pixel compare Still very slow

--- src/DupCheck.java
+++ src/DupCheck.java
@@ -1,3 +1,4 @@
+import javax.activation.MimetypesFileTypeMap;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
 import javax.imageio.metadata.IIOMetadata;
@@ -27,7 +28,9 @@ public class DupCheck {
         // for each true, update
         // a map of each file as the key and a string of results as the value
         // then come up with a clever way to process the map to output
-        String path = "/Users/scott/data/workspace/test/";
+
+//        String path = "/Users/scotts/data/workspace/test/";
+        String path = "/Users/scotts/Mine/DCIM/";
         try {
             System.out.println(checkForDuplicates(path));
         } catch (IOException e) {
@@ -36,26 +39,83 @@ public class DupCheck {
     }
     public static boolean checkForDuplicates(String path) throws IOException {
         File picsDir = new File(path);
-        String[] pictureSource = picsDir.list();
         boolean dupfound = false;
-        for(int i = 0; i < pictureSource.length; ++i) {
-            for(int u = i + 1; u < pictureSource.length; ++u) {
-//            System.out.println(pics[i]);
 
-                BufferedImage image1 = ImageIO.read(new File(path + pictureSource[i]));
-                BufferedImage image2 = ImageIO.read(new File(path + pictureSource[u]));
+        if(picsDir.exists()){
+            String[] listOfFilesToCheck = picsDir.list();
 
-                // check the size, only continue if the size is identical
-                if(isSameSize(image1, image2)) {
-                    dupfound = isExactDuplicate(image1, image2);
-                    System.out.println("Checking " + path + pictureSource[i] + " vs " + path + pictureSource[u]);
-                    System.out.println("Verdict: " + dupfound);
+            for(int i = 0; i < listOfFilesToCheck.length; ++i) {
+                for(int u = i + 1; u < listOfFilesToCheck.length; ++u) {
+
+//                    long startTime = System.nanoTime();
+//
+//                    long endTime = System.nanoTime();
+//                    long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+
+//                    System.out.println("resolves as image "+stringResolvesAsImage(listOfFilesToCheck[u]) + " " + stringResolvesAsImage(listOfFilesToCheck[i]));
+                    long startTime = System.nanoTime();
+
+                    boolean stringsResolve = stringResolvesAsImage(listOfFilesToCheck[u]) && stringResolvesAsImage(listOfFilesToCheck[i]);
+                    long endTime = System.nanoTime();
+                    long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+
+                    if(stringsResolve) {
+
+
+//                        System.out.println("goodtogo for: " + listOfFilesToCheck[i] + " " + listOfFilesToCheck[u]);
+                        File file1 = new File(path + listOfFilesToCheck[i]);
+                        File file2 = new File(path + listOfFilesToCheck[u]);
+
+                        BufferedImage image1 = ImageIO.read(file1);
+                        BufferedImage image2 = ImageIO.read(file2);
+
+//                        System.out.println("issame "+isSameSize(image1, image2));
+                        // check the size, only continue if the size is identical
+                        if(isSameSize(image1, image2)) {
+                            dupfound = isExactDuplicate(image1, image2);
+                            // this if statement is commented out to print failures as well as successes
+                            if(dupfound) {
+                                System.out.println("Dup " + path + listOfFilesToCheck[i] + " == " + path + listOfFilesToCheck[u]);
+//                                System.out.println("Verdict: " + dupfound);
+                            } else {
+//                                System.out.println("NOT Dup " + path + listOfFilesToCheck[i] + " == " + path + listOfFilesToCheck[u]);
+                            }
+                        }
+                    }
+
                 }
             }
+        } else {
+            System.out.println("Invalid Directory");
         }
         return dupfound;
     }
+
+    // named to be readable with statement "if (stringResolvesAsImage)"
+    public static boolean stringResolvesAsImage(String file) {
+        long startTime = System.nanoTime();
+
+        boolean retVal = false;
+        String validTypes = "jpeg, jpg, tiff, gif, png, bmp";
+        int fileExtensionLocation = file.lastIndexOf('.');
+        if(fileExtensionLocation > 0) {
+            String extension = file.substring(fileExtensionLocation + 1);
+            if(validTypes.toLowerCase().contains(extension.toLowerCase())) {
+                retVal = true;
+            } else {
+                System.out.println("extension: "+extension+" is not valid image type.");
+            }
+        }
+
+        long endTime = System.nanoTime();
+        System.out.println("stringResolvesAsImage took "+ (endTime - startTime));  //divide by 1000000 to get milliseconds.
+
+        return retVal;
+    }
+
     public static boolean isSameSize(BufferedImage image1, BufferedImage image2) {
+
+//        System.out.println(image1.getHeight() +""+ image2.getHeight() +""+ image1.getWidth() +""+ image2.getWidth());
         // return true if sizes are identical
         if(image1.getHeight() == image2.getHeight() && image1.getWidth() == image2.getWidth()) {
             return true;
@@ -64,6 +124,7 @@ public class DupCheck {
     }
 
     public static boolean isExactDuplicate(BufferedImage image1, BufferedImage image2) {
+        long startTime = System.nanoTime();
 
         for(int x = 0; x < image1.getWidth(); ++x) {
             for(int y = 0; y < image1.getHeight(); ++y) {
@@ -72,6 +133,9 @@ public class DupCheck {
                 int pixel1 = image1.getRGB(x, y);
                 int pixel2 = image2.getRGB(x, y);
                 if(pixel1 != pixel2) {
+                    long endTime = System.nanoTime();
+                    System.out.println("isExactDuplicate FALSE took "+ (endTime - startTime));  //divide by 1000000 to get milliseconds.
+
                     return false;
                 }
 
@@ -86,6 +150,9 @@ public class DupCheck {
 //                System.out.println("Blue Color value = "+ blue);
             }
         }
+        long endTime = System.nanoTime();
+        System.out.println("isExactDuplicate TRUE took "+ (endTime - startTime));  //divide by 1000000 to get milliseconds.
+
         return true;
     }
 
