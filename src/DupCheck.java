b84d537d6d7680e8521c50a4b180519619bede13
Scott Steinhorst <scott.steinhorst@gmail.com>
2016-02-05 23:01:03 -0700

    Working version with timing code added

--- src/DupCheck.java
+++ src/DupCheck.java
@@ -30,7 +30,9 @@ public class DupCheck {
         // then come up with a clever way to process the map to output
 
 //        String path = "/Users/scotts/data/workspace/test/";
-        String path = "/Users/scotts/Mine/DCIM/";
+//        String path = "/Users/scotts/Mine/DCIM/";
+        String path = "/Users/scotts/Mine/wallpaper/";
+
         try {
             System.out.println(checkForDuplicates(path));
         } catch (IOException e) {
@@ -41,9 +43,11 @@ public class DupCheck {
         File picsDir = new File(path);
         boolean dupfound = false;
 
+        int totalCompares = 0;
+        long startTime, endTime;
         if(picsDir.exists()){
             String[] listOfFilesToCheck = picsDir.list();
-
+            System.out.println("directory holds " + listOfFilesToCheck.length+" files");
             for(int i = 0; i < listOfFilesToCheck.length; ++i) {
                 for(int u = i + 1; u < listOfFilesToCheck.length; ++u) {
 
@@ -53,63 +57,63 @@ public class DupCheck {
 //                    long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
 
 //                    System.out.println("resolves as image "+stringResolvesAsImage(listOfFilesToCheck[u]) + " " + stringResolvesAsImage(listOfFilesToCheck[i]));
-                    long startTime = System.nanoTime();
+                    startTime = System.nanoTime();
 
                     boolean stringsResolve = stringResolvesAsImage(listOfFilesToCheck[u]) && stringResolvesAsImage(listOfFilesToCheck[i]);
-                    long endTime = System.nanoTime();
-                    long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+                    endTime = System.nanoTime();
+//                    System.out.println("stringResolveAsImage took :"+ ((endTime - startTime)/1000000));  //divide by 1000000 to get milliseconds.
 
                     if(stringsResolve) {
+                        startTime = System.nanoTime();
 
-
-//                        System.out.println("goodtogo for: " + listOfFilesToCheck[i] + " " + listOfFilesToCheck[u]);
                         File file1 = new File(path + listOfFilesToCheck[i]);
                         File file2 = new File(path + listOfFilesToCheck[u]);
 
                         BufferedImage image1 = ImageIO.read(file1);
                         BufferedImage image2 = ImageIO.read(file2);
 
-//                        System.out.println("issame "+isSameSize(image1, image2));
+                        endTime = System.nanoTime();
+                        System.out.println("Opening files took :"+ ((endTime - startTime)/1000000)+" ms");  //divide by 1000000 to get milliseconds.
+
                         // check the size, only continue if the size is identical
                         if(isSameSize(image1, image2)) {
+                            startTime = System.nanoTime();
+
                             dupfound = isExactDuplicate(image1, image2);
-                            // this if statement is commented out to print failures as well as successes
+                            endTime = System.nanoTime();
+                            System.out.println("isExactDuplicate took :"+ (endTime - startTime) + " ns");  //divide by 1000000 to get milliseconds.
+
+                            ++totalCompares;
                             if(dupfound) {
                                 System.out.println("Dup " + path + listOfFilesToCheck[i] + " == " + path + listOfFilesToCheck[u]);
-//                                System.out.println("Verdict: " + dupfound);
                             } else {
 //                                System.out.println("NOT Dup " + path + listOfFilesToCheck[i] + " == " + path + listOfFilesToCheck[u]);
                             }
                         }
                     }
-
                 }
             }
         } else {
             System.out.println("Invalid Directory");
         }
+        System.out.println("total compares: "+totalCompares);
         return dupfound;
     }
 
     // named to be readable with statement "if (stringResolvesAsImage)"
     public static boolean stringResolvesAsImage(String file) {
-        long startTime = System.nanoTime();
 
         boolean retVal = false;
-        String validTypes = "jpeg, jpg, tiff, gif, png, bmp";
+        String validTypes = "jpeg, jpg, tiff, png, bmp";
         int fileExtensionLocation = file.lastIndexOf('.');
         if(fileExtensionLocation > 0) {
             String extension = file.substring(fileExtensionLocation + 1);
             if(validTypes.toLowerCase().contains(extension.toLowerCase())) {
                 retVal = true;
             } else {
-                System.out.println("extension: "+extension+" is not valid image type.");
+//                System.out.println("extension: "+extension+" is not valid image type.");
             }
         }
-
-        long endTime = System.nanoTime();
-        System.out.println("stringResolvesAsImage took "+ (endTime - startTime));  //divide by 1000000 to get milliseconds.
-
         return retVal;
     }
 
@@ -124,7 +128,6 @@ public class DupCheck {
     }
 
     public static boolean isExactDuplicate(BufferedImage image1, BufferedImage image2) {
-        long startTime = System.nanoTime();
 
         for(int x = 0; x < image1.getWidth(); ++x) {
             for(int y = 0; y < image1.getHeight(); ++y) {
@@ -133,9 +136,6 @@ public class DupCheck {
                 int pixel1 = image1.getRGB(x, y);
                 int pixel2 = image2.getRGB(x, y);
                 if(pixel1 != pixel2) {
-                    long endTime = System.nanoTime();
-                    System.out.println("isExactDuplicate FALSE took "+ (endTime - startTime));  //divide by 1000000 to get milliseconds.
-
                     return false;
                 }
 
@@ -150,8 +150,6 @@ public class DupCheck {
 //                System.out.println("Blue Color value = "+ blue);
             }
         }
-        long endTime = System.nanoTime();
-        System.out.println("isExactDuplicate TRUE took "+ (endTime - startTime));  //divide by 1000000 to get milliseconds.
 
         return true;
     }
