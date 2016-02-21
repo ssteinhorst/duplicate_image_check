28188c7289bf3a494a231b4544e92ddf13b54231
Scott Steinhorst <scott.steinhorst@gmail.com>
2016-02-21 13:05:40 -0700

    Fast working version. Checks for duplicate images in the directory that is passed as a parameter.

--- src/DupCheck.java
+++ src/DupCheck.java
@@ -1,17 +1,5 @@
-import javax.activation.MimetypesFileTypeMap;
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.metadata.IIOMetadata;
-import javax.imageio.stream.ImageInputStream;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -22,46 +10,20 @@ public class DupCheck  {
 
     public static void main(String[] args){
 
-        //            getpixels(args[0]);
-        // inside a loop that compares each item in the list to
-        // each other, compare each picture and if a dup is found,
-        // add to a map of key,value
-
-        // so
-
-        // for each 2 pictures:
-        // determine if images are the same size
-        // no = no dup
-        // yes means we check pixels
-        // check pixel call takes 2 BufferedImage objects
-        // and returns true/false
-        // for each true, update
-        // a map of each file as the key and a string of results as the value
-        // then come up with a clever way to process the map to output
-
 //        String path = "/Users/scotts/data/workspace/test/";
-        String path = "/Users/scotts/Mine/DCIM/";
+//        String path = "/Users/scotts/Mine/DCIM/";
 //        String path = "/Users/scotts/Mine/wallpaper/";
-
+        String path = args[0];
         try {
-            System.out.println(checkForDuplicates(path));
+            checkForDuplicates(path);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static boolean checkForDuplicates(String path) throws IOException {
+    public static void checkForDuplicates(String path) throws IOException {
         File picsDir = new File(path);
-        boolean dupfound = false;
 
-
-//        final BlockingQueue<File> queue = new ArrayBlockingQueue<File>(1000);
-
-
-
-        ExecutorService executor = Executors.newFixedThreadPool(1);
-
-
-        BufferedImage img1;
+        final BlockingQueue<ImageCompareThread> queue = new ArrayBlockingQueue<ImageCompareThread>(10000000);
 
         int totalCompares = 0;
 //        long startTime, endTime;
@@ -70,21 +32,13 @@ public class DupCheck  {
             System.out.println("directory holds " + listOfFilesToCheck.length + " files");
             for(int i = 0; i < listOfFilesToCheck.length; ++i) {
 
-
                 if (stringResolvesAsImage(listOfFilesToCheck[i])) {
-                    File file1 = new File(path + listOfFilesToCheck[i]);
-                    img1 = ImageIO.read(file1);
-
-//                    img1 = ImageIO.read(Files.newInputStream(Paths.get(path + listOfFilesToCheck[i])));
 
                     for (int u = i + 1; u < listOfFilesToCheck.length; ++u) {
-//                        boolean stringsResolve = stringResolvesAsImage(listOfFilesToCheck[u]) && stringResolvesAsImage(listOfFilesToCheck[i]);
-
                         if (stringResolvesAsImage(listOfFilesToCheck[u])) {
 //                            System.out.println("Checking: " + listOfFilesToCheck[i] + " " + listOfFilesToCheck[u]);
-                            ArrayList<String> filesToCompare = new ArrayList<String>(Arrays.asList(Arrays.copyOfRange(listOfFilesToCheck, u, listOfFilesToCheck.length)));
-//                            executor.submit(new Thread(new ImageCompareThread(path, img1, listOfFilesToCheck[i], listOfFilesToCheck[u], filesToCompare, totalCompares)));
-                            executor.execute( new ImageCompareThread(path, img1, listOfFilesToCheck[i], listOfFilesToCheck[u], filesToCompare, totalCompares) );
+                            queue.add( new ImageCompareThread(path, listOfFilesToCheck[i], listOfFilesToCheck[u]) );
+                            ++totalCompares;
                         }
                     }
                 }
@@ -92,9 +46,25 @@ public class DupCheck  {
         } else {
             System.out.println("Invalid Directory");
         }
-        executor.shutdown();
+
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+
+        for(int i = 0; i < 5; i++){
+            Runnable r = new Runnable(){
+                public void run() {
+                    ImageCompareThread workFile;
+                    while((workFile = queue.poll()) != null){
+                        //work on the file.
+//                        System.out.println("starting check of files "+workFile.image1name+" "+workFile.image2name);
+                        workFile.check();
+                    }
+                }
+            };
+            pool.execute(r);
+        }
+        pool.shutdown();
+
         System.out.println("total compares: "+totalCompares);
-        return dupfound;
     }
 
     // named to be readable with statement "if (stringResolvesAsImage)"
@@ -113,98 +83,5 @@ public class DupCheck  {
         }
         return retVal;
     }
-
-//    public static boolean isSameSize(BufferedImage image1, BufferedImage image2) {
-//
-////        System.out.println(image1.getHeight() +""+ image2.getHeight() +""+ image1.getWidth() +""+ image2.getWidth());
-//        // return true if sizes are identical
-//        if(image1.getHeight() == image2.getHeight() && image1.getWidth() == image2.getWidth()) {
-//            return true;
-//        }
-//        return false;
-//    }
-
-//    public static boolean isExactDuplicate(BufferedImage image1, BufferedImage image2) {
-//
-//        for(int x = 0; x < image1.getWidth(); ++x) {
-//            for(int y = 0; y < image1.getHeight(); ++y) {
-////                System.out.println(image.getRGB(x, y));
-//
-//                int pixel1 = image1.getRGB(x, y);
-//                int pixel2 = image2.getRGB(x, y);
-//                if(pixel1 != pixel2) {
-//                    return false;
-//                }
-//
-////                Color color = new Color(pixel);
-////                System.out.println("pixel "+color.toString());
-//
-////                int red = (pixel & 0x00ff0000) >> 16;
-////                int green = (pixel & 0x0000ff00) >> 8;
-////                int blue  =  pixel & 0x000000ff;
-////                System.out.println("Red Color value = "+ red);
-////                System.out.println("Green Color value = "+ green);
-////                System.out.println("Blue Color value = "+ blue);
-//            }
-//        }
-//
-//        return true;
-//    }
-
-    public static double getMSE(String pic1, String pic2) throws IOException {
-        double retVal = 0;
-        File file1= new File(pic1);
-        BufferedImage image1 = ImageIO.read(file1);
-        File file2= new File(pic1);
-        BufferedImage image2 = ImageIO.read(file2);
-        for(int x = 0; x < image1.getWidth(); ++x) {
-            for(int y = 0; y < image1.getHeight(); ++y) {
-//                total
-            }
-        }
-        return retVal;
-    }
-
-    // debug function, outputs all the pixel values for the image at the given path
-    public static void getpixels(String arg) throws IOException {
-        File file= new File(arg);
-        BufferedImage image = ImageIO.read(file);
-        // outputs all the pixel vales of the given image.
-        // this overloads the buffer ATM so you probably dont want to run it for large images
-        for(int x = 0; x < image.getWidth(); ++x) {
-            for(int y = 0; y < image.getHeight(); ++y) {
-//                System.out.println(image.getRGB(x, y));
-
-                int pixel = image.getRGB(x,y);
-                Color color = new Color(pixel);
-                System.out.println("pixel "+color.toString());
-
-//                int red = (pixel & 0x00ff0000) >> 16;
-//                int green = (pixel & 0x0000ff00) >> 8;
-//                int blue  =  pixel & 0x000000ff;
-//                System.out.println("Red Color value = "+ red);
-//                System.out.println("Green Color value = "+ green);
-//                System.out.println("Blue Color value = "+ blue);
-            }
-        }
-    }
 }
 
-//    //Calculates the MSE between two images
-//    private double MSE(Bitmap original, Bitmap enhanced)
-//    {
-//        Size imgSize = original.Size;
-//        double total = 0;
-//
-//        for (int y = 0; y < imgSize.Height; y++)
-//        {
-//            for (int x = 0; x < imgSize.Width; x++)
-//            {
-//                total += System.Math.Pow(original.GetPixel(x, y).R - enhanced.GetPixel(x, y).R, 2);
-//
-//            }
-//
-//        }
-//
-//        return (total / (imgSize.Width * imgSize.Height));
-//    }
--- src/ImageCompareThread.java
+++ src/ImageCompareThread.java
@@ -2,66 +2,66 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 
-public class ImageCompareThread implements Runnable {
-    private BufferedImage image1;
-    private String image2name;
-    private String image1name;
+public class ImageCompareThread{
+    public String image1name;
+    public String image2name;
     private String path;
-    private ArrayList<String> listOfFilesToCheck;
-    private int totalCompares;
 
-    public ImageCompareThread(String pth, BufferedImage img1, String img1name, String img2, ArrayList<String> fileList, int totalCompares) {
+    public ImageCompareThread(String pth, String img1name, String img2name) {
         this.path = pth;
-        this.image1 = img1;
-        this.image2name = img2;
         this.image1name = img1name;
-        this.totalCompares = totalCompares;
-        //unused yet
-        this.listOfFilesToCheck = fileList;
+        this.image2name = img2name;
     }
-    @Override
-    public void run() {
+
+    public void check() {
 
         boolean dupfound;
+//        long startTime, endTime;
 
-        // wrap this in a loop and loop through all the compares in this one
-
-
-//        BufferedImage image1 = null;
+        BufferedImage image1 = null;
         BufferedImage image2 = null;
-//            image1 = ImageIO.read(Files.newInputStream(Paths.get(this.path + this.image1)));
         try {
-//            image2 = ImageIO.read(Files.newInputStream(Paths.get(this.path + this.image2name)));
+
+//            startTime = System.nanoTime();
+
+            File file1 = new File(this.path + this.image1name);
             File file2 = new File(this.path + this.image2name);
+
+//            endTime = System.nanoTime();
+//            System.out.println("creating file objects took :"+ (endTime - startTime) + " ns");  //divide by 1000000 to get milliseconds.
+
+            if(file1.length() != file2.length()) {
+                return;
+            }
+
+//            startTime = System.nanoTime();
+
+            image1 = ImageIO.read(file1);
             image2 = ImageIO.read(file2);
 
+//            endTime = System.nanoTime();
+//            System.out.println("creating Image objects took :"+ (endTime - startTime) + " ns");  //divide by 1000000 to get milliseconds.
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-//        endTime = System.nanoTime();
-//                        System.out.println("Opening files NEW STYLE took :"+ ((endTime - startTime)/1000000)+" ms");  //divide by 1000000 to get milliseconds.
-
-//                        System.out.println("");
-        // check the size, only continue if the size is identical
+        // check the resolution, only continue if it is identical
         if(isSameSize(image1, image2)) {
 //            startTime = System.nanoTime();
 
             dupfound = isExactDuplicate(image1, image2);
 //            endTime = System.nanoTime();
-//                            System.out.println("isExactDuplicate took :"+ (endTime - startTime) + " ns");  //divide by 1000000 to get milliseconds.
+//            System.out.println("isExactDuplicate took :"+ (endTime - startTime) + " ns");  //divide by 1000000 to get milliseconds.
 
-//            ++totalCompares;
             if(dupfound) {
                 System.out.println("Dup " + path + this.image1name + " == " + path + this.image2name);
             } else {
-                System.out.println("NOT Dup " + path + this.image1name + " == " + path + this.image2name);
+//                System.out.println("NOT Dup " + path + this.image1name + " == " + path + this.image2name);
             }
+        } else {
+//            System.out.println(this.image1name + " - "+this.image2name+" are not same size");
         }
     }
 
@@ -76,14 +76,15 @@ public class ImageCompareThread implements Runnable {
     }
 
     private boolean isExactDuplicate(BufferedImage image1, BufferedImage image2) {
-
-        for(int x = 0; x < image1.getWidth(); ++x) {
-            for(int y = 0; y < image1.getHeight(); ++y) {
+        int x, y;
+        for(x = 0; x < image1.getWidth(); ++x) {
+            for(y = 0; y < image1.getHeight(); ++y) {
 //                System.out.println(image.getRGB(x, y));
 
                 int pixel1 = image1.getRGB(x, y);
                 int pixel2 = image2.getRGB(x, y);
                 if(pixel1 != pixel2) {
+//                    System.out.println("isExactDuplicate x:"+x+" y:"+y);
                     return false;
                 }
 
@@ -98,7 +99,7 @@ public class ImageCompareThread implements Runnable {
 //                System.out.println("Blue Color value = "+ blue);
             }
         }
-
+//        System.out.println("isExactDuplicate x:"+x+" y:"+y);
         return true;
     }
 }
